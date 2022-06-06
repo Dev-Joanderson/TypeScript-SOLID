1153a385b56d218a040aa3e1e2e50e94e6a6f767
joanderson <trajanojoanderson@gmail.com>
2022-06-05 22:30:15 -0300

    Shopping Carts ??

--- src/srp/shopping-cart.ts
+++ src/srp/shopping-cart.ts
@@ -1,7 +1,9 @@
 type CartItem = { name: string; price: number };
+type orderStatus = 'open' | 'closed';
 
 export class ShoppingCart {
   private readonly _items: CartItem[] = [];
+  private _orderStatus: orderStatus = 'open';
 
   addItem(item: CartItem): void {
     this._items.push(item);
@@ -14,11 +16,49 @@ export class ShoppingCart {
   get items(): Readonly<CartItem[]> {
     return this._items;
   }
+
+  get orderStatus(): orderStatus {
+    return this._orderStatus;
+  }
+  total(): number {
+    return +this._items
+      .reduce((total, next) => total + next.price, 0)
+      .toFixed(2);
+  }
+  checkout(): void {
+    if (this.isEmpty()) {
+      console.log('Checkout');
+      return;
+    }
+
+    this._orderStatus = 'closed';
+    this.senMessage('Seu pedido  foi recebido');
+    this.saveOrder();
+    this.clear();
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0;
+  }
+
+  senMessage(msg: string): void {
+    console.log('Messagem enviada:', msg);
+  }
+  saveOrder(): void {
+    console.log('Salvo com sucesso...');
+  }
+  clear(): void {
+    console.log('Carrinho de compras foi limpo...');
+    this._items.length = 0;
+  }
 }
 
 const shoppingCart = new ShoppingCart();
 shoppingCart.addItem({ name: 'Camisa', price: 49.9 });
 shoppingCart.addItem({ name: 'blase', price: 25.9 });
-shoppingCart.addItem({ name: 'calça', price: 8.9 });
+shoppingCart.addItem({ name: 'calça', price: 8.933 });
 
 console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+shoppingCart.checkout();
+console.log(shoppingCart.orderStatus);
